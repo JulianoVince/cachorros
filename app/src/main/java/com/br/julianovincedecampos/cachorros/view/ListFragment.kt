dc2c15871d1f803344b27a49dcb62ce78df26e51
Juliano Vince De Campos <juliano.campos@mercadolivre.com>
2020-12-14 16:46:36 -0300

    refactore gradle module e project. add func Main onSupportNavigateUp back action, finsh fragments nav and layout

--- app/src/main/java/com/br/julianovincedecampos/cachorros/view/ListFragment.kt
+++ app/src/main/java/com/br/julianovincedecampos/cachorros/view/ListFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.br.julianovincedecampos.cachorros.R
+import kotlinx.android.synthetic.main.fragment_list.*
 
 class ListFragment : Fragment() {
 
@@ -13,5 +15,13 @@ class ListFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        buttonDetalis.setOnClickListener {
+            val action = ListFragmentDirections.actionDetailFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }
