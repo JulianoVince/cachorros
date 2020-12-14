ec99d95e8577bf29041ecded8c3d601c17ca4405
Juliano Vince De Campos <juliano.campos@mercadolivre.com>
2020-12-14 14:53:14 -0300

    Criação de framents, navigation

--- app/src/main/java/com/br/julianovincedecampos/cachorros/view/MainActivity.kt
+++ app/src/main/java/com/br/julianovincedecampos/cachorros/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.br.julianovincedecampos.cachorros
+package com.br.julianovincedecampos.cachorros.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.br.julianovincedecampos.cachorros.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
