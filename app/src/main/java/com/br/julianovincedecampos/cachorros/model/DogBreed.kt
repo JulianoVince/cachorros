20880090e7c2cdc31277d09e95634606c0a05174
Juliano Vince De Campos <juliano.campos@mercadolivre.com>
2020-12-16 10:32:49 -0300

    Criando a comunicação com o servidor

--- app/src/main/java/com/br/julianovincedecampos/cachorros/model/DogBreed.kt
+++ app/src/main/java/com/br/julianovincedecampos/cachorros/model/DogBreed.kt
@@ -1,11 +1,28 @@
 package com.br.julianovincedecampos.cachorros.model
 
+import com.google.gson.annotations.SerializedName
+
 data class DogBreed(
+
+    @SerializedName("id")
     val breedId: String?,
+
+    @SerializedName("name")
     val dogBreed: String?,
+
+    @SerializedName("life_span")
     val lifeSpan: String?,
+
+    @SerializedName("breed_group")
     val breedGroup: String,
+
+    @SerializedName("bread_for")
     val breadFor: String,
+
+    @SerializedName("temperament")
     val temperament: String,
+
+    @SerializedName("url")
     val imageUrl: String
+
 )
